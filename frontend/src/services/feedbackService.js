34956dbb427f771543408f88da29f9742cc21fb5
Chandran <223081227@ge.com>
2026-07-07 05:25:29 +0530

    Gallery added png pics

--- frontend/src/services/feedbackService.js
+++ frontend/src/services/feedbackService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { API_BASE_URL } from "../config/api";
 
-const API = API_BASE_URL;
+const API = `${API_BASE_URL}/feedback`;
 
 // =========================
 // Get All Feedback
