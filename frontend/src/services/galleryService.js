6a2fdf9c63991190271726cd7dc352055a401467
Chandran <223081227@ge.com>
2026-07-07 05:02:15 +0530

    Gallery service changed

--- frontend/src/services/galleryService.js
+++ frontend/src/services/galleryService.js
@@ -1,46 +1,34 @@
 import axios from "axios";
 import { API_BASE_URL } from "../config/api";
-const API =API_BASE_URL;
+
+const API = `${API_BASE_URL}/gallery`;
 
 // =======================
 // Get Gallery
 // =======================
 
 export const getGallery = async () => {
-
-  const response =
-    await axios.get(API);
-
+  const response = await axios.get(API);
   return response.data;
-
 };
 
 // =======================
 // Add Image
 // =======================
 
-export const addGalleryImage = async (
-  image,
-  caption
-) => {
-
-  const response =
-    await axios.post(API, {
-      image,
-      caption,
-    });
+export const addGalleryImage = async (image, caption) => {
+  const response = await axios.post(API, {
+    image,
+    caption,
+  });
 
   return response.data;
-
 };
 
 // =======================
 // Delete Image
 // =======================
 
-export const deleteGalleryImage =
-  async (id) => {
-
-    await axios.delete(`${API}/${id}`);
-
+export const deleteGalleryImage = async (id) => {
+  await axios.delete(`${API}/${id}`);
 };
